d7c1f603c5987a74f5962975dc6cb1beb5e342c2
Rümeysa Yiğit <yigtrumeysa16@gmail.com>
2020-05-22 02:39:12 +0300

    ...

---
+++ GU.TusasKazanMyo.KitapeviSistemiApp/Kitap.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GU.TusasKazanMyo.KitapeviSistemiApp
+{
+    class Kitap
+    {
+        public Kitap()
+        {
+
+        }
+
+        public Kitap(string kitapad, string yazarad, DateTime tarih, string tur)
+        {
+            this.Kitapad = kitapad;
+            this.Yazarad = yazarad;
+            this.Tarih = tarih;
+            this.Tur = tur;
+
+        }
+
+        public static void DosyaKayitIslemi()
+        {
+
+
+            DateTime Tarih = DateTime.Now.Date;
+
+            Console.WriteLine("Kitap adı:");
+            string Kitapad = Console.ReadLine();
+            Console.WriteLine("Yazarın Adı:");
+            string Yazarad = Console.ReadLine();
+
+            Tarih = Kitap.getTarih();
+
+            int sonuc = DateTime.Compare(DateTime.Now.Date, Tarih);
+            if (sonuc < 0)
+            {
+                Console.WriteLine("Basım tarihi 2020'den büyük olamaz.");
+                Tarih = Kitap.getTarih();
+            }
+
+            Console.WriteLine("Kitabın Türü:");
+            string Tur = Console.ReadLine();
+            Console.WriteLine("***");
+
+            try
+            {
+                FileStream fs = new FileStream(@"C:\Kitapevi Sistemi\liste.txt", FileMode.Append);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.Write($"Kitabın Adı:{Kitapad},Kitabın Yazarı:{Yazarad},Basım Tarihi:{Tarih},Kitabın Türü:{Tur}\r\n");
+                fs.Flush();
+                sw.Close();
+                fs.Close();
+                Console.WriteLine("Eklediğiniz kitaplar kaydedildi.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Eklediğiniz kitaplar kaydedilmedi.");
+
+            }
+
+
+        }
+
+        public static DateTime getTarih()
+        {
+            CultureInfo tarih_ = CultureInfo.InvariantCulture;
+
+            try
+            {
+                Console.WriteLine("Basım Tarihi:");
+                DateTime Tarih = DateTime.Parse(Console.ReadLine());
+
+                return Tarih;
+            }
+            catch
+            {
+                Console.WriteLine("Tarih formatını kontrol ediniz...");
+                Console.WriteLine("Basım Tarihi (Format: dd-mm-yyyy veya dd/mm/yyyy ):");
+                DateTime Tarih = DateTime.Parse(Console.ReadLine());
+                return Tarih;
+
+            }
+        }
+
+
+        string kitapad;
+        string yazarad;
+        DateTime tarih;
+        string tur;
+
+
+
+        public string Kitapad
+        {
+            get
+            {
+                return kitapad;
+            }
+
+            set
+
+            {
+                kitapad = value;
+            }
+        }
+        public string Yazarad
+        {
+
+            get
+            {
+                return yazarad;
+            }
+            set
+            {
+                yazarad = value;
+            }
+        }
+        public DateTime Tarih
+        {
+            get
+            {
+                return tarih;
+            }
+            set
+            {
+                tarih = value;
+            }
+        }
+        public string Tur
+        {
+            get
+            {
+                return tur;
+            }
+            set
+            {
+                tur = value;
+            }
+        }
+    }
+}
---
+++ GU.TusasKazanMyo.KitapeviSistemiApp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GU.TusasKazanMyo.KitapeviSistemiApp
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A) Kitap Ekle         B) Kitapevindeki kitapları listele");
+            Console.WriteLine("Bir komut ekleyiniz! Kitap eklemek için a'yı, kitap listelemek için b'yi");
+            string ekle = "a";
+            string listele = "b";
+
+            string menu = Console.ReadLine();
+
+
+            if (menu == ekle)
+            {
+
+                try
+                {
+                    Console.WriteLine("Kaç tane kitap girmek istiyorsunuz?");
+                    int adet = int.Parse(Console.ReadLine());
+
+                    int[] adet_ = new int[adet];
+
+                    for (int i = 0; i < adet; i++)
+                    {
+                         Kitap.DosyaKayitIslemi();
+                    }
+
+                   
+                }
+                catch (FormatException)
+                {
+                    
+                    Console.WriteLine("Lütfen rakam giriniz!");
+                    Console.WriteLine("Kaç tane kitap girmek istiyorsunuz?");
+                    int adet = int.Parse(Console.ReadLine());
+                    int[] adet_ = new int[adet];
+
+                    for (int i = 0; i < adet; i++)
+                    {
+                        Kitap.DosyaKayitIslemi();
+                    }
+                }
+
+            }
+
+            else if (menu == listele)
+            {
+
+                StreamReader sr = new StreamReader(@"C:\Kitapevi Sistemi\liste.txt");
+                Console.WriteLine(sr.ReadToEnd());
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
